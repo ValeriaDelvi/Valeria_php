1e4dab70e0a459685d8d3f7e9f27f5d756df555c
ValeriaDelvi <byvale@live.it>
2024-06-04 09:49:26 +0200

    tabella

---
+++ tabella.php
@@ -0,0 +1,12 @@
+<table style="width:80%">
+    <tr>
+     <td colspan="2">OFFERTA</td>
+     <td colspan="2">DOMANDA</td>
+    </tr>
+    <tr>
+      <td rowspan="2">Quantità</td>
+      <td>prezzo</td>
+      <td rowspan="2">Quantità</td>
+      <td>prezzo</td>
+    </tr>
+</table>
